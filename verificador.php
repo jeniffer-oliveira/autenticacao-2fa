baae2c12b76574aac095e2dcc5b8ad575c1cb2b0
Jeniffer Oliveira <100139970+jeniffer-oliveira@users.noreply.github.com>
2022-05-25 22:31:19 -0300

    Update verificador.php

--- verificador.php
+++ verificador.php
@@ -21,4 +21,17 @@ else{
 }
 ?>
 
-<a href="index.php">Voltar</a>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Verificador </title>
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/2fa.css">
+    <body>
+        <a href="index.php">Voltar</a>
+    </body>
+</head>
+</html>
