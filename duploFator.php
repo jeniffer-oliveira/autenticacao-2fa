5271cd9e5fdd6234cf6c4f2a97a21d0bff5e7804
Jeniffer Oliveira <100139970+jeniffer-oliveira@users.noreply.github.com>
2022-05-25 22:37:05 -0300

    Update duploFator.php

--- duploFator.php
+++ duploFator.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela Login</title>
+    <title>Login</title>
     <link href="style.css" rel="stylesheet">
     <link rel="stylesheet" href="css/2fa.css">
 </head>
